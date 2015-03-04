28ec31b1193efe960069bc32c27afff038adeeb0
biroa <adam.biro@gmail.com>
2015-03-04 22:05:07 +0000

    ucfirst only

--- config/app.php
+++ config/app.php
@@ -194,8 +194,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
-        'Form'      => 'Illuminate\HTML\FormFacade',
-        'Html'      => 'Illuminate\HTML\HtmlFacade',
+        'Form'      => 'Illuminate\Html\FormFacade',
+        'Html'      => 'Illuminate\Html\HtmlFacade',
 
 	],
 
