979bf0c0c472cd7e0645986d13dc1db09ab2f5ee
biroa <adam.biro@gmail.com>
2015-03-08 13:18:32 +0000

    add new route create new Controller

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+
+    public function index(){
+        return 'articles will be here';
+    }
+
+
+}
