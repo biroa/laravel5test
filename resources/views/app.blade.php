7cdc03721d66a45d58b6eb3b07f1f02e5149a488
biroa <adam.biro@gmail.com>
2015-03-16 12:18:36 +0000

    fix typo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
 <div class="container">
     @if(Session::has('flash_message'))
         <div class="alert alert-success">
-            <button type="button" class="close" data-dissmiss="alert" aria-hidden="true">&times;</button>
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             {{ Session::get('flash_message') }}
         </div>
     @endif
