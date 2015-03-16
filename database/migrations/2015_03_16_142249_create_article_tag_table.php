95687f37d6bd7d09c879485f8eee26fd7f47ab4b
biroa <adam.biro@gmail.com>
2015-03-16 14:24:56 +0000

    rename to  article_tag

--- database/migrations/2015_03_16_142249_create_article_tag_table.php
+++ database/migrations/2015_03_16_142249_create_article_tag_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleTagsTable extends Migration {
+class CreateArticleTagTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateArticleTagsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('article_tags', function(Blueprint $table)
+		Schema::create('article_tag', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->timestamps();
@@ -26,7 +26,7 @@ class CreateArticleTagsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('article_tags');
+		Schema::drop('article_tag');
 	}
 
 }
