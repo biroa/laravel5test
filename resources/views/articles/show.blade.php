e7a471fe8f05c8f776d91e7922efe28a3b3e9541
biroa <adam.biro@gmail.com>
2015-03-17 11:03:21 +0000

    change view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,4 +5,11 @@
             <h2>{{$article->title}}</h2>
             <div class="body">{{$article->body}}</div>
         </article>
+
+    <h5>Tags:</h5>
+        <ul>
+            @foreach($article->tag as $tag)
+                <li>{{$tag->name}}</li>
+            @endforeach
+        </ul>
 @stop
