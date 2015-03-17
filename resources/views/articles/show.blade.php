993f79aa5d4373809231ba5a30ee919a4ba4599c
biroa <adam.biro@gmail.com>
2015-03-17 11:08:12 +0000

    add unless to view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,10 +6,12 @@
             <div class="body">{{$article->body}}</div>
         </article>
 
+    @unless($article->tag->isEmpty())
     <h5>Tags:</h5>
         <ul>
             @foreach($article->tag as $tag)
                 <li>{{$tag->name}}</li>
             @endforeach
         </ul>
+    @endunless
 @stop
