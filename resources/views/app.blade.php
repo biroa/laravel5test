5a6cf05c37e8a42355535b0bd999ceb9d4c611d9
biroa <adam.biro@gmail.com>
2015-03-19 21:16:20 +0000

    add js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/css/select2.min.css" rel="stylesheet" />
     <![endif]-->
 </head>
 <body>
@@ -26,6 +27,7 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/js/select2.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
 @yield('footer')
