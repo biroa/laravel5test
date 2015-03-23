88079c23ca1ae09752b3f2325b30332232ba212c
biroa <adam.biro@gmail.com>
2015-03-23 21:01:56 +0000

    reformat

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -48,8 +48,9 @@ class TagsController extends Controller
      */
     public function show(Tag $tag)
     {
-        $articles =  $tag->articles()->published()->get();
-        return view('articles.index',compact('articles'));
+        $articles = $tag->articles()->published()->get();
+
+        return view('articles.index', compact('articles'));
     }
 
     /**
