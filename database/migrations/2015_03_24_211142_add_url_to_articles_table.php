fb03a77f90078d651e2a399b3904bda8e13aec13
biroa <adam.biro@gmail.com>
2015-03-24 21:17:52 +0000

    add new field to the articles table

--- database/migrations/2015_03_24_211142_add_url_to_articles_table.php
+++ database/migrations/2015_03_24_211142_add_url_to_articles_table.php
@@ -1,34 +1,33 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class AddUrlToArticlesTable extends Migration {
+class AddUrlToArticlesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('articles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->text('url')->nullable();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('articles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('url');
+        });
+    }
 
 }
