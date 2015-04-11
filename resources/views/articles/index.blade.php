3e188d14ac6129d6d4d795b29a306548cfae6fd0
biroa <adam.biro@gmail.com>
2015-04-11 20:44:17 +0100

    add table to Articles edit in blade

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,13 +3,48 @@
 @section('content')
     <h1>Articles</h1>
 
-    @foreach($articles as $article)
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading" translate>
+                    user_page.SLUG_USER_LIST
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>title</th>
+                                <th>excerpt</th>
+                                <th>url</th>
+                                <th>category</th>
+                                <th>edit</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($articles as $article)
 
-        <article>
-            <h2>
-                <a href="{{  url('/articles',$article->id) }}">{{$article->title}}</a>
-            </h2>
-            <div class="body">{{$article->body}}</div>
-        </article>
-    @endforeach
+                                <tr>
+                                    <td>{{$article->id}}</td>
+                                    <td>{{$article->title}}</td>
+                                    <td>{{$article->excerpt}}</td>
+                                    <td>{{$article->url}}</td>
+                                    <td>{{$article->category->name}}</td>
+                                    <td><a href=<?='/articles/' . $article->id . '/edit' ?> ><span class="glyphicon glyphicon-pencil"></span></a><span>&nbsp;</span><span class="glyphicon glyphicon-remove""></span></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
 @stop
