f044da90142b7c491cd99b68844f3ce921914070
biroa <adam.biro@gmail.com>
2015-04-13 22:49:59 +0100

    fix table head

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -7,7 +7,7 @@
         <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    List of Categories
+                    List of Tags
                 </div>
                 <!-- /.panel-heading -->
                 <div class="panel-body">
