8e9859fa775aab233d22c0c52e1f76923536b827
biroa <adam.biro@gmail.com>
2015-04-13 22:25:39 +0100

    add index blade of category && call the view file

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,50 @@
+@extends('app')
+
+@section('content')
+    <h1>Categories</h1>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    List of Categories
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>name</th>
+                                <th>short_lead</th>
+                                <th>updated</th>
+                                <th>created</th>
+                                <th>edit</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($categories as $category)
+
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>{{$category->short_lead}}</td>
+                                    <td>{{$category->updated_at}}</td>
+                                    <td>{{$category->created_at}}</td>
+                                    <td><a href=<?='/articles/' . $category->id . '/edit' ?> ><span class="glyphicon glyphicon-pencil"></span></a><span>&nbsp;</span><span class="glyphicon glyphicon-remove""></span></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+@stop
