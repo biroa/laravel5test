8e96c68afbdcd7f09a5c2287999ea9954d920f64
biroa <adam.biro@gmail.com>
2015-04-16 21:44:00 +0100

    form_blade partials for edit categories

---
+++ resources/views/categories/partials/_form.blade.php
@@ -0,0 +1,34 @@
+<div class="form-group">
+    {!! Form::label('name','Category name') !!}
+    {!! Form::text('name',null,['class'=>'form-control']) !!}
+</div>
+
+
+<div class="form-group">
+    {!! Form::label('short_lead','Short Lead') !!}
+    {!!
+    Form::text('short_lead',$category->short_lead,['class'=>'form-control'])
+    !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('lead','Lead') !!}
+    {!! Form::textarea('body',$category->lead,['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('description','Description') !!}
+    {!! Form::textarea('description',$category->description,['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('created_at','Created at') !!}
+    {!!
+    Form::input('date','created_at',$category->created_at,['class'=>'form-control'])
+    !!}
+</div>
+
+<div class="form-group">
+    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary
+    form-control']) !!}
+</div>
