8192f9ce2bb862f77f62146bd303efb20ee93ad6
biroa <adam.biro@gmail.com>
2015-04-18 19:41:00 +0100

    mod template of categories and tags

--- resources/views/categories/partials/_form.blade.php
+++ resources/views/categories/partials/_form.blade.php
@@ -21,12 +21,6 @@
     {!! Form::textarea('description',$category->description,['class'=>'form-control']) !!}
 </div>
 
-<div class="form-group">
-    {!! Form::label('created_at','Created at') !!}
-    {!!
-    Form::input('date','created_at',$category->created_at,['class'=>'form-control'])
-    !!}
-</div>
 
 <div class="form-group">
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary
--- resources/views/tags/partials/_form.blade.php
+++ resources/views/tags/partials/_form.blade.php
@@ -4,13 +4,6 @@
 </div>
 
 <div class="form-group">
-    {!! Form::label('created_at','Created at') !!}
-    {!!
-    Form::input('date','created_at',$tag->created_at,['class'=>'form-control'])
-    !!}
-</div>
-
-<div class="form-group">
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary
     form-control']) !!}
 </div>
