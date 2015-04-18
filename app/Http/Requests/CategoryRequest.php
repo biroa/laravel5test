12ca9d67882d4617f86496754d58deb9a81925fc
biroa <adam.biro@gmail.com>
2015-04-18 20:38:12 +0100

    tag&category request class rename

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Requests;
 
-class CreateCategoryRequest extends Request
+class CategoryRequest extends Request
 {
 
     /**
--- app/Http/Requests/TagRequest.php
+++ app/Http/Requests/TagRequest.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Requests;
 
-class CreateTagRequest extends Request
+class TagRequest extends Request
 {
 
     /**
