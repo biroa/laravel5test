38af9576d6cb12b84c5659699d237f7dd28d2e04
biroa <adam.biro@gmail.com>
2015-05-03 17:15:17 +0100

    mod index blades

---
+++ resources/views/userprofiles/index.blade.php
@@ -0,0 +1,34 @@
+@extends('app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-6 text-left">
+            <h1>Users' Profiles</h1>
+        </div>
+        <div class="col-lg-6 text-right">
+            <a href="/articles/create" class="btn btn-primary" role="button">Create New User</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    List of Users' Profiles
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="table-responsive">
+
+                    </div>
+                    <!-- /.table-responsive -->
+
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+@stop
