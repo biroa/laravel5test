182373007a40acfdf6eac2d36edbb5e17e7d5089
biroa <adam.biro@gmail.com>
2015-05-05 19:02:33 +0100

    add avatar field to user profile

--- database/migrations/2015_05_05_175621_add_avatar_to_userprofiles_table.php
+++ database/migrations/2015_05_05_175621_add_avatar_to_userprofiles_table.php
@@ -1,34 +1,33 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class AddAvatarToUserprofilesTable extends Migration {
+class AddAvatarToUserprofilesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('userprofiles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('userprofiles', function (Blueprint $table) {
+            $table->string('avatar')->after('country_id');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('userprofiles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('userprofiles', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+        });
+    }
 
 }
