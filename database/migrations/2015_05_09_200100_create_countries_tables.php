ce8774ae5bf90878ba9f00b0d1b7906255f3854d
biroa <adam.biro@gmail.com>
2015-05-09 21:18:26 +0100

    create countries

--- database/migrations/2015_05_09_200100_create_countries_tables.php
+++ database/migrations/2015_05_09_200100_create_countries_tables.php
@@ -1,32 +1,35 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateCountriesTables extends Migration {
+class CreateCountriesTables extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('countries', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255)->unique('name');
+            $table->string('country_code_2', 2)->unique('code_2');
+            $table->string('country_code_3', 3)->unique('code_3');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('countries');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('countries');
+    }
 
 }
