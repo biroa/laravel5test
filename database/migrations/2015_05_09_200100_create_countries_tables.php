b8707a1219775af5cbbbf95d9d4212c90160dfdc
biroa <adam.biro@gmail.com>
2015-05-10 12:20:43 +0100

    countries migration file

--- database/migrations/2015_05_09_200100_create_countries_tables.php
+++ database/migrations/2015_05_09_200100_create_countries_tables.php
@@ -16,8 +16,9 @@ class CreateCountriesTables extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255)->unique('name');
-            $table->string('country_code_2', 2)->unique('code_2');
-            $table->string('country_code_3', 3)->unique('code_3');
+            $table->string('nicename', 255)->unique('nicename');
+            $table->string('country_code_2', 2);
+            $table->string('country_code_3', 3);
             $table->timestamps();
         });
     }
