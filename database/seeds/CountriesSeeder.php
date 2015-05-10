a64b197b9a3fc8003642b87f595b4dde2b86d40b
biroa <adam.biro@gmail.com>
2015-05-10 12:56:57 +0100

    reformat seeder and prepare the countries seeder

--- database/seeds/CountriesSeeder.php
+++ database/seeds/CountriesSeeder.php
@@ -3,10 +3,11 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
 
     /**
+     * Todo::We have to write the countries seeder here !!!
+     *
      * Run the database seeds.
      *
      * @return void
