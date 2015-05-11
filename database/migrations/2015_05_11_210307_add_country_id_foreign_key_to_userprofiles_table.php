6913c87de81f426c3e71d2a1520d6e6fe71cf2bb
biroa <adam.biro@gmail.com>
2015-05-11 22:04:19 +0100

    countrd_id as foreign key

---
+++ database/migrations/2015_05_11_210307_add_country_id_foreign_key_to_userprofiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCountryIdForeignKeyToUserprofilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('userprofiles', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('userprofiles', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
