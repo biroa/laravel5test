2a079a3d4c728855bdaf44ef86fab2556d14ad4e
biroa <adam.biro@gmail.com>
2015-05-11 22:13:28 +0100

    add foreign key to the country_id

--- database/migrations/2015_05_11_210307_add_country_id_foreign_key_to_userprofiles_table.php
+++ database/migrations/2015_05_11_210307_add_country_id_foreign_key_to_userprofiles_table.php
@@ -1,34 +1,35 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class AddCountryIdForeignKeyToUserprofilesTable extends Migration {
+class AddCountryIdForeignKeyToUserprofilesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('userprofiles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('userprofiles', function (Blueprint $table) {
+            $table->integer('country_id')->unsigned()->index()->change();
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('userprofiles', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('userprofiles', function (Blueprint $table) {
+            $table->dropForeign('country_id');
+            $table->dropColumn('country_id');
+        });
+    }
 
 }
