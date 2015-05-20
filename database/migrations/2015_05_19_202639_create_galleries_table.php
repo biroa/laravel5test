e52b74977f3c0ee87ee3a53572c967d9dadbf12d
biroa <adam.biro@gmail.com>
2015-05-20 19:42:57 +0100

    migration galleries table

--- database/migrations/2015_05_19_202639_create_galleries_table.php
+++ database/migrations/2015_05_19_202639_create_galleries_table.php
@@ -1,32 +1,42 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateGalleriesTable extends Migration {
+class CreateGalleriesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('galleries', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->string('name', 150);
+            $table->string('thumbnail', 255);
+            $table->integer('lg_width', 150);
+            $table->integer('lg_height', 150);
+            $table->integer('m_width', 150);
+            $table->integer('m_height', 150);
+            $table->integer('sm_width', 150);
+            $table->integer('sm_height', 150);
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('galleries');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('galleries');
+    }
 
 }
