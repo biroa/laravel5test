f5dd94caf706b871b9f2451ab7666d38138d049f
biroa <adam.biro@gmail.com>
2015-05-22 21:34:31 +0100

    set relation: image belongs to gallery

--- app/Image.php
+++ app/Image.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model {
 
-	//
+class Image extends Model
+{
+
+    /**
+     * relation
+     *
+     * Image belongs to one Gallery
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function  gallery(){
+       return $this->belongsTo('App\Gallery');
+    }
 
 }
