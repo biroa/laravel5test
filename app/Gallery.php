77104f07a8e9f01a35310e62ab8ea95b4257b8d5
biroa <adam.biro@gmail.com>
2015-05-23 19:44:43 +0100

    getAll galleries With and Without relation methods

--- app/Gallery.php
+++ app/Gallery.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
 
+    /**
+     * get All galleries with the correspondent images
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function scopeGetAllWith(){
+        return $this->with('images')->get();
+    }
+
+    /**
+     * get All galleries without relations
+     *
+     * @return mixed
+     */
+    public function scopeGetAllWithout()
+    {
+        return $this->get();
+    }
 
     /**
      * relation
