a0b7b850b3ffae81eda9653636f7fac0db8a3898
biroa <adam.biro@gmail.com>
2015-06-08 19:59:46 +0100

    categorizable fields

--- database/migrations/2015_06_08_184149_add_polymorphic_fields_to_the_categories_table.php
+++ database/migrations/2015_06_08_184149_add_polymorphic_fields_to_the_categories_table.php
@@ -1,34 +1,35 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class AddPolymorphicFieldsToTheCategoriesTable extends Migration {
+class AddPolymorphicFieldsToTheCategoriesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('categories', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('categorizable_id');
+            $table->string('categorizable_type');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('categories', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('categorizable_id');
+            $table->dropColumn('categorizable_type');
+        });
+    }
 
 }
