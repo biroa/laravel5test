68e7fceb228363a9b5c38831170d32e3503db656
biroa <adam.biro@gmail.com>
2015-06-15 19:10:35 +0100

    image blade

--- resources/views/images/partials/_form.blade.php
+++ resources/views/images/partials/_form.blade.php
@@ -1,4 +1,36 @@
 <div class="form-group">
+    {!! Form::label('name','Image title') !!}
+    {!! Form::text('name',$images->name,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('description','Description') !!}
+    {!! Form::text('description',$images->description,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
     {!! Form::label('Image') !!}
     {!! Form::file('image', null) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('Camera','camera') !!}
+    {!! Form::text('camera',$images->camera,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('lens','Lens') !!}
+    {!! Form::text('lens',$images->lens,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('focal_length','Focal length') !!}
+    {!! Form::text('focal_length',$images->focal_length,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('shutter_speed','Shutter speed') !!}
+    {!! Form::text('shutter_speed',$images->shutter_speed,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('aperture','Aperture') !!}
+    {!! Form::text('aperture',$images->aperture,['class'=>'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('iso','Iso') !!}
+    {!! Form::text('iso',$images->iso,['class'=>'form-control']) !!}
 </div>
