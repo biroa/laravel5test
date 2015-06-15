681fc211377953ab0ee0e414b60f7bb5212b45ca
biroa <adam.biro@gmail.com>
2015-06-15 19:44:34 +0100

    add images

--- resources/views/images/partials/_form.blade.php
+++ resources/views/images/partials/_form.blade.php
@@ -34,3 +34,8 @@
     {!! Form::label('iso','Iso') !!}
     {!! Form::text('iso',$images->iso,['class'=>'form-control']) !!}
 </div>
+
+<div class="form-group">
+    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary
+    form-control']) !!}
+</div>
