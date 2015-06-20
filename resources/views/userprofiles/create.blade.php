5f853867ac1a248d01cb5c0006d7a8e11f1344f7
biroa <adam.biro@gmail.com>
2015-06-20 11:16:50 +0100

    mod userprofile  blades

--- resources/views/userprofiles/create.blade.php
+++ resources/views/userprofiles/create.blade.php
@@ -4,9 +4,10 @@
     <h1>New Article</h1>
 
     <hr/>
+    @include('errors.list')
+
     {!! Form::model($userprofile = new \App\Userprofile,['url'=> 'userprofiles' ]) !!}
     @include('userprofiles.partials._form',['submitButtonText'=> 'Update User profile'])
     {!! Form::close() !!}
 
-    @include('errors.list')
 @stop
--- resources/views/userprofiles/edit.blade.php
+++ resources/views/userprofiles/edit.blade.php
@@ -4,12 +4,10 @@
     <h1>Edit user: {!! $userprofile->first_name .' '. $userprofile->last_name !!}</h1>
 
     <hr/>
+    @include('errors.list')
 
     {!! Form::model($userprofile ,['method' => 'PATCH',  'action' => ['UserprofilesController@update' , $userprofile->id ] ]) !!}
     @include('userprofiles.partials._form',['submitButtonText'=> 'Update User profile'])
     {!! Form::close() !!}
 
-
-
-    @include('errors.list')
 @stop
