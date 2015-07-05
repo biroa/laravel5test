1837a95b9a3dc8dfe87bae84f204f2ef01615f24
biroa <adam.biro@gmail.com>
2015-07-05 15:55:22 +0100

    modification

--- app/Http/Traits/ImageEditor.php
+++ app/Http/Traits/ImageEditor.php
@@ -4,18 +4,66 @@ namespace App\Http\Traits;
 
 use Intervention\Image\ImageManager;
 
-trait ImageEditor{
+trait ImageEditor
+{
 
-    public function createFolderIfNotExists()
+    protected $folders =
+        [
+            'original' => [
+                'img',
+                'thumbnails'
+            ],
+            'processed' => [
+                'img',
+                'thumbnails'
+            ]
+
+        ];
+
+    protected function createFolder($folderPath)
     {
-        $folderPath = env('GALLERY_THUMBNAIL_PATH');
-        if ( !file_exists($folderPath) ) {
-            if ( !mkdir($folderPath, 0777, true) ) {
-                Log::alert('I could not create the folder');
-            }
+        if ( !mkdir($folderPath, 0777, true) ) {
+            Log::alert('I could not create the folder');
         }
 
         return true;
+    }
+
+    /**
+     * @param array $folderPath
+     * @return array
+     */
+    public function removeLastItemIfEmpty(array $folderPath){
+        $lastItem = (int) count($folderPath)-1;
+        if(empty ($folderPath[$lastItem])){
+            array_pop($folderPath);
+        }
+
+        return $folderPath;
+    }
+
+    /**
+     * @param null $path
+     */
+    public function createFolderIfNotExists($path = null)
+    {
+        if ( is_null($path) ) {
+            $path = env('GALLERY_THUMBNAIL_PATH');
+        }
+
+        $segment = explode('/', $path);
+        dd($this->removeLastItemIfEmpty($segment));
+//        foreach ( $this->folders as $mainFolder ) {
+//            array_push($segments[$i],$mainFolder);
+//            array_push($segments[$i],$mainFolder);
+//            foreach($mainFolder as $subFolders){
+//                array_push($segments[$i],$subFolders);
+//                if ( !file_exists($path) ) {
+//
+//                }
+//
+//            }
+//        }
     }
 
     //Todo:: Temporary!! there will be a common resizer
