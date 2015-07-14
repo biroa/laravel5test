96a771d5ce3e859b9724cde695a6e551ab278381
biroa <adam.biro@gmail.com>
2015-07-14 20:35:38 +0100

    add orig_thumbnail field to gallery table

--- database/migrations/2015_07_14_192418_add_orig_thumbnail_to_galleries_table.php
+++ database/migrations/2015_07_14_192418_add_orig_thumbnail_to_galleries_table.php
@@ -1,34 +1,33 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class AddOrigThumbnailToGalleriesTable extends Migration {
+class AddOrigThumbnailToGalleriesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('galleries', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->string('orig_thumbnail', 255)->after('name');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('galleries', function(Blueprint $table)
-		{
-			//
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropColumn('orig_thumbnail');
+        });
+    }
 
 }
